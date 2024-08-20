1fcc85aec0c2a351971cccb9f7db9951d81132f4
YanivaVentures <yanivaventures@gmail.com>
2024-08-20 15:04:35 +0530

    first commit

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import img from '../images/web.svg'; 
-import img2 from '../images/app.svg';
-import img3 from '../images/hosting.svg';
-import img4 from '../images/consultation.svg';
+import img from '../images/pre.svg'; 
+import img2 from '../images/ipo.svg';
+import img3 from '../images/ADV.svg';
+import img4 from '../images/ma.svg';
+import img5 from '../images/PR.svg';
 
 const Services = () => {
 
@@ -24,9 +25,9 @@ const Services = () => {
                             <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
                                 <div className="m-2 text-justify text-sm">
                                 <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img} />
-                                    <h2 className="font-semibold my-4 text-2xl text-center">Web Development</h2>
+                                    <h2 className="font-semibold my-4 text-2xl text-center">Pre-IPO</h2>
                                     <p className="text-md font-medium">
-                                        We specialize in creating and optimizing high-quality, custom websites for businesses and organizations of all sizes. Building mobile-friendly and easy-to-use websites and applications for clients.
+                                    Pre-IPO investment refers to the process of investing in a company before it goes public through an Initial Public Offering (IPO). This type of investment allows investors to buy shares at a potentially lower price than the public offering price. Pre-IPO investments are typically available to institutional investors, venture capitalists, and accredited investors. The appeal of pre-IPO investment lies in the potential for high returns.
                                     </p>
                                 </div>
                             </div>
@@ -34,9 +35,9 @@ const Services = () => {
                             <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
                                 <div className="m-2 text-justify text-sm">
                                 <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img2} />
-                                    <h2 className="font-semibold my-4 text-2xl text-center">Mobile App Development</h2>
+                                    <h2 className="font-semibold my-4 text-2xl text-center">IPO Management</h2>
                                     <p className="text-md font-medium">
-                                        We develop high-quality, custom cross-platform mobile applications that are robust and optimized for performance, scalability, and accessibility.
+                                    IPO management involves overseeing the entire process of a company going public by offering its shares to the general public for the first time. It includes preparing the company for the IPO, conducting due diligence, and ensuring regulatory compliance with financial authorities like the SEBI. Key steps include selecting underwriters, setting the offer price, and marketing the IPO to potential investors.
                                     </p>
                                 </div>
                             </div>
@@ -44,9 +45,9 @@ const Services = () => {
                             <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
                                 <div className="m-2 text-justify text-sm">
                                     <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img3} />
-                                    <h2 className="font-semibold my-4 text-2xl text-center ">Domain and Hosting Services</h2>
+                                    <h2 className="font-semibold my-4 text-2xl text-center ">Financial Advisory</h2>
                                     <p className="text-md font-medium">
-                                        We provide domain registration and web hosting services to individuals and organizations to enable them gain visibility in the digital space.
+                                    Corporate financial advisory services offer businesses expert guidance on financial strategies, including mergers and acquisitions, capital raising, and restructuring. These advisors help companies optimize their financial structures, manage risks, and maximize shareholder value. We provide crucial insights on business valuations, due diligence, and deal negotiations, ensuring informed decision-making. By aligning financial strategies with corporate goals, advisors play a key role in guiding companies through complex transactions like IPOs, debt financing, and major investments.  
                                     </p>
                                 </div>
                             </div>
@@ -54,12 +55,22 @@ const Services = () => {
                             <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
                                 <div className="m-2 text-justify text-sm">
                                 <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img4} />
-                                    <h2 className="font-semibold my-4 text-2xl text-center ">General IT Consultations</h2>
+                                    <h2 className="font-semibold my-4 text-2xl text-center ">Merger and Aquisition</h2>
                                     <p className="text-md font-medium">
-                                        Our IT consultations service provides expert guidance and support for businesses and individuals looking to optimize their technology solutions.
+                                    Mergers and acquisitions (M&A) consulting provides expert guidance to companies involved in buying, selling, or merging businesses. Consultants assist with strategy development, target identification, and valuation to ensure that deals align with the company’s objectives. They conduct thorough due diligence, assess financial and operational risks, and help negotiate terms to maximize value for their clients. M&A consultants also manage the integration process post-transaction, ensuring a smooth transition and the realization of synergies. Their expertise is critical in navigating the complexities of M&A transactions, from initial planning to successful execution.
                                     </p>
                                 </div>
-                            </div>                    
+                            </div> 
+
+                            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                                <div className="m-2 text-justify text-sm">
+                                <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img5} />
+                                    <h2 className="font-semibold my-4 text-2xl text-center ">PR and Marketing</h2>
+                                    <p className="text-md font-medium">
+                                    PR and marketing consulting for an IPO involves crafting a strategic communication plan to build investor confidence and public interest ahead of the company's public offering. Consultants help shape the company’s narrative, highlighting its strengths, growth potential, and market position to attract potential investors. They manage media relations, create compelling messaging, and coordinate investor roadshows to generate buzz. Additionally, these consultants work to ensure regulatory compliance in all communications, minimizing risks while maximizing visibility. Effective PR and marketing strategies are essential for a successful IPO, helping to set the stage for a strong market debut.
+                                    </p>
+                                  </div>
+                            </div>
                         </div>
                     </div>
             </section>
@@ -69,23 +80,22 @@ const Services = () => {
                     <div className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left" data-aos="zoom-out">
                         <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
                             <div className='text-blue-900 mb-4'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24" className='fill-current'><path d="M2 12h2a7.986 7.986 0 0 1 2.337-5.663 7.91 7.91 0 0 1 2.542-1.71 8.12 8.12 0 0 1 6.13-.041A2.488 2.488 0 0 0 17.5 7C18.886 7 20 5.886 20 4.5S18.886 2 17.5 2c-.689 0-1.312.276-1.763.725-2.431-.973-5.223-.958-7.635.059a9.928 9.928 0 0 0-3.18 2.139 9.92 9.92 0 0 0-2.14 3.179A10.005 10.005 0 0 0 2 12zm17.373 3.122c-.401.952-.977 1.808-1.71 2.541s-1.589 1.309-2.542 1.71a8.12 8.12 0 0 1-6.13.041A2.488 2.488 0 0 0 6.5 17C5.114 17 4 18.114 4 19.5S5.114 22 6.5 22c.689 0 1.312-.276 1.763-.725A9.965 9.965 0 0 0 12 22a9.983 9.983 0 0 0 9.217-6.102A9.992 9.992 0 0 0 22 12h-2a7.993 7.993 0 0 1-.627 3.122z"></path><path d="M12 7.462c-2.502 0-4.538 2.036-4.538 4.538S9.498 16.538 12 16.538s4.538-2.036 4.538-4.538S14.502 7.462 12 7.462zm0 7.076c-1.399 0-2.538-1.139-2.538-2.538S10.601 9.462 12 9.462s2.538 1.139 2.538 2.538-1.139 2.538-2.538 2.538z"></path></svg>
-                            </div>
+                                                            </div>
                             <h3 className="text-3xl  text-blue-900 
-                            font-bold">We <span className='font-black'>Build</span></h3>
+                            font-bold"> <span className='font-black'></span></h3>
                             <div>
                                 <p className='my-3 text-xl text-gray-600 font-semibold'>
-                                    With over 10 years experience in software analysis and design and a deep understanding of the latest IT trends and solutions, we provide customized recommendations and strategies to help you improve your operations, reduce costs, and increase efficiency.
+                                
                                 </p>
                             </div>
                         </div>
                         <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
                             <div className='text-blue-900 mb-4'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24" className='fill-current'><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg>
+                                
                             </div>
-                            <h3 className="text-3xl  text-blue-900 font-bold">We <span className='font-black'>Collaborate</span></h3>
+                            <h3 className="text-3xl  text-blue-900 font-bold"> <span className='font-black'></span></h3>
                             <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>We can collaborate with your existing tech team to scale existing software applications or design customized software applications that suits your everyday need and simplifies various processes.</p>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'></p>
                             </div>
                         </div>
                     </div>
